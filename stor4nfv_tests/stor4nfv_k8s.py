df1e6e3f64c220afa397c8856fa4fd4f20853a0b
Qiaowei Ren <qiaowei.ren@intel.com>
2018-03-20 13:44:22 +0800

    Add k8s test case
    Change-Id: I7ef6fd6aa31fae47cca446d31b6b54d13454df9c
    Signed-off-by: Qiaowei Ren <qiaowei.ren@intel.com>

---
+++ stor4nfv_tests/stor4nfv_k8s.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2018 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+import sys
+import os
+import subprocess
+
+import functest_kubernetes.k8stest as k8stest
+
+
+class K8sStor4nfvTest(k8stest.K8sTesting):
+
+    """Stor4NFV Kubernetes test suite"""
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs.get("case_name", 'stor4nfv_k8s')
+        super(K8sStor4nfvTest, self).__init__(**kwargs)
+        self.check_envs()
+        self.cmd = ['/src/k8s.io/kubernetes/cluster/validate-cluster.sh']
