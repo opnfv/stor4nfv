4e9176dad81964318224be71d25fd8ee75dac179
Cédric Ollivier <cedric.ollivier@orange.com>
2018-09-15 15:45:36 +0200

    Remove check_envs call
    It was removed because it was useless [1].
    [1] https://build.opnfv.org/ci/view/functest/job/functest-compass-virtual-daily-fraser/772/console
    
    Change-Id: I7620e959f756bb77e65dd1f770ecdb40e7042629
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 6b637d11c99450ed0a1f3b9a5231ccea82e5cd5b)

--- stor4nfv_tests/stor4nfv_k8s.py
+++ stor4nfv_tests/stor4nfv_k8s.py
@@ -22,5 +22,4 @@ class K8sStor4nfvTest(k8stest.K8sTesting):
         if "case_name" not in kwargs:
             kwargs.get("case_name", 'stor4nfv_k8s')
         super(K8sStor4nfvTest, self).__init__(**kwargs)
-        self.check_envs()
         self.cmd = ['/src/k8s.io/kubernetes/cluster/validate-cluster.sh']
